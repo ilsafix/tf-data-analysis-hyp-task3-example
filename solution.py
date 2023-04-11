a5ba9fcbe486384d064b8416f57f91010358ce3a
ilsafix <55927732+ilsafix@users.noreply.github.com>
2023-04-11 18:18:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ def solution(control_sample, test_sample) -> bool: # Одна или две вы
     n1 = len(control_sample)
     n2 = len(test_sample)
     x1_bar = np.sum(control_sample) / n1
-    x2_bar = mp.sum(test_sample) / n2
+    x2_bar = np.sum(test_sample) / n2
     s1 = np.sum([(x - x1_bar)**2 for x in control_sample]) / (n1 - 1)
     s2 = np.sum([(x - x2_bar)**2 for x in test_sample]) / (n2 - 1)
     sp = ((n1 - 1) * s1 + (n2 - 1) * s2) / (n1 + n2 - 2)
