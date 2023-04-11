a11f39baf6b3f2d4ba897e57666a3237f094c0d8
ilsafix <55927732+ilsafix@users.noreply.github.com>
2023-04-11 17:35:41 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import ttest_ind, t
 
 chat_id = 391223586 # Ваш chat ID, не меняйте название переменной
 
