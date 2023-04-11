ed5f7bac0f319ee0b88394d44340945457a22043
ilsafix <55927732+ilsafix@users.noreply.github.com>
2023-04-11 17:51:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,27 +1,18 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind, t
+from scipy.stats import t
 
 chat_id = 391223586 # Ваш chat ID, не меняйте название переменной
 
-def solution(control_npv, test_npv) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(control_sample, test_sample) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     alpha = 0.04
-    # calculate the sample means and standard deviations
-    control_mean = np.mean(control_npv)
-    test_mean = np.mean(test_npv)
-    control_std = np.std(control_npv, ddof=1)
-    test_std = np.std(test_npv, ddof=1)
-
-    # calculate the t-statistic and p-value
-    t, p = ttest_ind(test_npv, control_npv, equal_var=False)
-
-    # calculate the degrees of freedom
-    df = len(control_npv) + len(test_npv) - 2
-
-    # calculate the critical t-value for the given significance level
-    t_crit = np.abs(ttest_ind(control_npv, test_npv)[0])
-    t_dist = t(df=df)
-    t_dist_alpha = t_dist.ppf(1 - alpha)
-    reject_null = t_crit > t_dist_alpha
-    
-    return reject_null
+    n1 = len(control_sample)
+    n2 = len(test_sample)
+    x1_bar = np.sum(control_sample) / n1
+    x2_bar = mp.sum(test_sample) / n2
+    s1 = np.sum([(x - x1_bar)**2 for x in control_sample]) / (n1 - 1)
+    s2 = np.sum([(x - x2_bar)**2 for x in test_sample]) / (n2 - 1)
+    sp = ((n1 - 1) * s1 + (n2 - 1) * s2) / (n1 + n2 - 2)
+    t_statistic = (x2_bar - x1_bar) / (sp * (1/n1 + 1/n2)**0.5)
+    t_critical = t.ppf(1 - alpha/2, n1 + n2 - 2)
+    return np.abs(t_statistic) > t_critical
